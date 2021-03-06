70a1f18aba768afdb70b758f6777915fde2a1d43
Adarsh <adarshmishra9654@gmail.com>
2021-03-06 19:25:54 +0530

    newchanges

---
+++ Lec1/introduction.js
@@ -0,0 +1,23 @@
+//let a =10;
+//let b = "hey";
+//let c = true;
+//let d;
+//console.log(a);
+//console.log(b);
+//console.log(c);
+//console.log(d);
+//const pi = 3.141;
+//console.log(pi);
+//let data = {
+  //  name : "adarsh",
+    //section : "placements",
+//}
+//console.log(data);
+//console.log(data.name);
+//console.log(data.section);
+//let key = "name ";
+//console.log(data[key]);
+//for in loops 
+//for(let key in data ){
+    //console.log(key);
+//}
