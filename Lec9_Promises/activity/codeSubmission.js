24eedc71756b916a926219517c78d83747373d9e
Adarsh <adarshmishra9654@gmail.com>
2021-03-26 21:11:46 +0530

    Automation 1

---
+++ Lec9_Promises/activity/codeSubmission.js
@@ -0,0 +1,43 @@
+const puppeteer = require("puppeteer");
+const id = "casiyod230@aramidth.com";
+const pw = "1234567890";
+let tab;
+
+let browserOpenPromise = puppeteer.launch({
+  headless: false,
+  defaultViewport: null,
+  args: ["--start-maximized"],
+  slowMo : 100
+});
+
+
+browserOpenPromise
+  .then(function (browser) {
+    console.log("Browser opened !!!");
+    let allPagesPromise = browser.pages();
+    return allPagesPromise;
+  })
+  .then(function (pages) {
+    tab = pages[0];
+    let pageOpenPromise = tab.goto("https://www.hackerrank.com/auth/login");
+    return pageOpenPromise;
+
+  })
+  .then(function () {
+      let idTypePromise = tab.type("#input-1" , id);
+      return idTypePromise;
+  })
+  .then(function(){
+      let pwTypePromise = tab.type("#input-2" , pw);
+      return pwTypePromise;
+  })
+  .then(function(){
+      let loginPromise = tab.click('.ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled');
+      return loginPromise;
+  })
+  .then(function(){
+      console.log("logged in to hackerrank !!!");
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
